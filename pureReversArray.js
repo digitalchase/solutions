fd9f1f337c2d4cc5e8b2406dd6efa55c4d0281c9
xuryrg2034 <32922511+xuryrg2034@users.noreply.github.com>
2019-04-22 02:06:09 +0700

    Функция для разворота массива, но без мутации входящего массива

---
+++ pureReversArray.js
@@ -0,0 +1,13 @@
+//Проблема метода Array.prototype.reverse()в том, что он изменяет массив который мы пытаемся развернуть, а это не очень хорошо, ведь в коде где то дальше, нам может понадобится тот же самый массив, но не перевернутый
+
+//Поэтому вот вариант функции через Array.prototype.reduce();
+
+let array = [0,1,2,3];
+
+function pureReverse(arr) {
+  let revArr = arr.reduce((ar, el)=> {
+    return [el, ...ar]
+  }, []);
+
+  return revArr;
+}
