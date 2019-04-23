d71ecd6a8816c81dc0e49d1f3064d03d18d25de6
xuryrg2034 <32922511+xuryrg2034@users.noreply.github.com>
2019-04-23 22:55:29 +0700

    Update currentBrowser.js

--- currentBrowser.js
+++ currentBrowser.js
@@ -11,3 +11,17 @@ function currentBrowser() {
     };
     $('body').addClass('ie');
 }
+
+//Самовызывющаяся функция
+(function () {
+    var ua = navigator.userAgent;
+    if (ua.match(/MSIE/)) return;
+    if (ua.match(/Firefox/)) return;
+    if (ua.match(/Opera/)) return;
+    if (ua.match(/Chrome/)) return;
+    if (ua.match(/Safari/)) {
+        $('html').attr('data-browser', 'safari'); //В нашем случае мы ищем сафари
+        return;
+    };
+    $('body').addClass('ie');
+})();
