f6c96b688814b295c87b4ff0e00bd4aad92f8159
xuryrg2034 <32922511+xuryrg2034@users.noreply.github.com>
2019-05-21 18:03:05 +0700

    Create stopScroll.js

---
+++ stopScroll.js
@@ -0,0 +1,22 @@
+
+
+var scrollTop = 0;
+function stopScroll(state) {
+    //Есди страница уже фиксирована, то вызов этой функции с аргументом state = true, ничего не сделает
+    if (state && $("html")[0].style.position == "fixed") {
+        return false;
+    }
+
+    if (state) {
+        scrollTop = $(window).scrollTop();
+        $("html").css({
+            position: "fixed",
+            top: -scrollTop
+        });
+    } else {
+        $("html").css({
+            position: "static"
+        });
+        $(window).scrollTop(scrollTop);
+    }
+}
