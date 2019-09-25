3fb3d3f658dc82a472c6abf561f11a6971785a2c
xuryrg2034 <32922511+xuryrg2034@users.noreply.github.com>
2019-09-25 13:02:36 +0700

    Update regExp.js

--- regExp.js
+++ regExp.js
@@ -2,3 +2,9 @@
 function proverka(input) {
   input.value = input.value.replace(/[^\d,]/g, '');
 };
+
+
+//Ввод только кириллицы
+function proverka(input) {
+  input.value = input.value.replace(/[^аА-яЯ]/,'');
+};
