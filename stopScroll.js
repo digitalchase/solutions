fabab69a1f3a34ad19d1bb73f3e3d9911178963d
xuryrg2034 <32922511+xuryrg2034@users.noreply.github.com>
2019-09-25 14:17:16 +0700

    Update stopScroll.js

--- stopScroll.js
+++ stopScroll.js
@@ -1,6 +1,7 @@
 
 
-var var scrollTopPositionInit = 0;
+//ES 5
+var scrollTopPositionInit = 0;
 function stopScroll(state) {
     //Есди страница уже фиксирована, то вызов этой функции с аргументом state = true, ничего не сделает
     if (state && $("html")[0].style.position == "fixed") {
@@ -20,3 +21,50 @@ function stopScroll(state) {
         $(window).scrollTop(scrollTopPositionInit);
     }
 }
+
+//ES 6
+class StopScroll {
+    constructor() {
+        this.scrollTopPositionInit = 0;
+        this.stopScrollIsActive = false;
+    }
+
+    scroll(flag) {
+        const that = this;
+        const body = document.querySelector("body"),
+            html = document.querySelector("html");
+
+        if (flag) {
+            html.setAttribute(
+                "style",
+                `
+                    position:static; 
+                `
+            );
+
+            if (that.stopScrollIsActive) {
+                window.scrollTo(0, that.scrollTopPositionInit);
+                that.stopScrollIsActive = false;
+            }
+        } else {
+            that.scrollTopPositionInit = window.pageYOffset;
+            html.setAttribute(
+                "style",
+                `
+                    position:fixed; 
+                    top: ${that.scrollTopPositionInit * -1}px;
+                `
+            );
+
+            that.stopScrollIsActive = true;
+        }
+    }
+}
+
+let stopScroll = new StopScroll();
+
+//Функция scroll принмает 1 аргумент (true / false)
+//true - скролл разрешен
+//false - скролл запрещен
+stopScroll.scroll()
+
