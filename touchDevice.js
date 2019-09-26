86239ac9bc341f8a6c1c882506e77e6f82855503
xuryrg2034 <32922511+xuryrg2034@users.noreply.github.com>
2019-09-26 11:45:00 +0700

    Update touchDevice.js

--- touchDevice.js
+++ touchDevice.js
@@ -1,4 +1,13 @@
-//Функция определения тач устройств 
+//Функция определения мобильная ли эта система или нет
 function checkTouchDevice() {
     return !!navigator.userAgent.match(/(iPhone|iPod|iPad|Android|playbook|silk|BlackBerry|BB10|Windows Phone|Tizen|Bada|webOS|IEMobile|Opera Mini)/);
+}
+
+//Функция определяет тач устроство
+function checkTouchDevice() {
+    if ("ontouchstart" in window || navigator.msMaxTouchPoints) {
+        return true;
+    } else {
+        return false;
+    }
 }
