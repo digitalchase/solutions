e36a34ce80ea1594baadc78ff5f7d4d0c8409173
xuryrg2034 <32922511+xuryrg2034@users.noreply.github.com>
2019-10-02 16:20:36 +0700

    Create activate.js

---
+++ numberMask/activate.js
@@ -0,0 +1,28 @@
+/*
+  --- HTML EXAMPLE ---
+  <input type="text" id="withAttr" _dc-data-type="phone" _dc-data-placeholder="asd" _dc-data-mask="+7 (___) ___ ____" _dc-data-cursor-move-end />
+  <input type="text" id="withOutAttr" />
+  <input type="text" id="defaultSettings" />
+  
+
+/*
+    options = {
+        mask: String //обязательный аргумент. Задаем вид маски "+7 (___) ___ ____" вместо нижнего подчеркивания подставятся цифры.
+                     //так же можно внести в атрибут _dc-data-mask="+7 (___) ___ ____"
+        cursorMoveEnd: Boolean // Поумолчанию false. true - При любом редактировании значения в input, переносит каретку в конец строки
+                               // Можно записать как data атрибу _dc-daata-cursor-move-end
+        placeholder: String // Так же можно внести это поле на самом элементе в атрибут _dc-data-placeholder
+    }
+*/
+
+var options = {
+  mask: "+7 (___) ___ ____",
+  cursorMoveEnd: true,
+  placeholder: "Введите номер"
+}
+
+
+new DcNumberMask('#withOutAttr', options); //Параметры из optiosn
+new DcNumberMask('#withAttr'); //Параметры из атрибутов
+new DcNumberMask('#defaultSettings'); //Стандартные настройки
+
