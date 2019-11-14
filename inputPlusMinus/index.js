e11c8e940f888b3f45594baabd9e42a9a7da3788
xuryrg2034 <32922511+xuryrg2034@users.noreply.github.com>
2019-11-14 14:11:25 +0700

    Create index.js

---
+++ inputPlusMinus/index.js
@@ -0,0 +1,34 @@
+if ($("[data-toggleInputContainer]").length) {
+  $("[data-toggleInputContainer]").each(function() {
+    const toggleBtn = $(this).find("[data-toggleInput]");
+    const input = $(this).find("input");
+    const step = (() => {
+      if (
+        input.data("step") === undefined ||
+        input.data("step") === "" ||
+        isNaN(Number(input.data("step")))
+      ) {
+        return 1;
+      } else {
+        return Number(input.data("step"));
+      }
+    })();
+
+    toggleBtn.on("click", function() {
+      switch ($(this).data("toggleinput")) {
+        case "plus":
+          input.val(Number(input.val()) + step);
+          break;
+        case "minus":
+          input.val(Number(input.val()) - step);
+          break;
+      }
+
+      if (isNaN(Number(input.val()))) {
+        input.val(0);
+      }
+
+      input.trigger("change");
+    });
+  });
+}
