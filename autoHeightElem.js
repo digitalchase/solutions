843e8e3ba53127620e629385d47d6fbd25b13956
xuryrg2034 <32922511+xuryrg2034@users.noreply.github.com>
2019-12-17 18:15:48 +0700

    Create autoHeightElem.js

---
+++ autoHeightElem.js
@@ -0,0 +1,12 @@
+//Находит элемент с максимальной высотой и выставляет ее все элементом с тем же классом
+
+//Принимает селектор элемента по которым будет проходиться
+const autoHeight = (selector) => {
+    const elems =  document.querySelectorAll(selector);
+    let itemHeight = Array.from(elems).reduce((accum, el) => {
+        let h = el.offsetHeight;
+        if (Number(h) > accum) accum = h;
+        return accum;
+    }, 0);
+    elems.forEach(el => el.style.height = `${itemHeight}px`);
+}
