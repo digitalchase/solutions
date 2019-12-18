58227aaf9e37ac4610059a82f0d35adb87375b3d
xuryrg2034 <32922511+xuryrg2034@users.noreply.github.com>
2019-12-18 17:13:54 +0700

    Update activate.js

--- numberMask/activate.js
+++ numberMask/activate.js
@@ -21,9 +21,10 @@ var options = {
   placeholder: "Введите номер"
 }
 
+var el = document.querySelector('#withOutAttr');
 
 // Первый аргумент принимает либо строку (селектор элемента), либо элемент Node
-new DcNumberMask('#withOutAttr', options); //Параметры из optiosn
+new DcNumberMask(el, options); //Параметры из optiosn
 new DcNumberMask('#withAttr'); //Параметры из атрибутов
 new DcNumberMask('#defaultSettings'); //Стандартные настройки
 
